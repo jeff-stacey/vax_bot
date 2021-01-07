8b5cf8c3b7bad58b4f07a2f64c48c4afe404455d
Ashlynn <ashlynn.steeves@icloud.com>
2021-01-06 16:49:09 -0800

    days to total vaccination

--- vax.rb
+++ vax.rb
@@ -20,7 +20,11 @@ new_vax = data["change_vaccinations"]
 total_vax = data["total_vaccinations"]
 percent_vax = total_vax.to_f / canada_population * 100
 
+remaining_vax = canada_population - total_vax
+days_to_total_vax = remaining_vax.to_f/new_vax
+days_to_total_vax = days_to_total_vax.ceil()
+date_of_total_vax = Date.today + days_to_total_vax
 
 puts("Today #{new_vax} people were vaccinated in Canada")
-
 puts("Overall, #{total_vax} people have been vaccinated. This is #{'%.2f' % percent_vax}% of the population.")
+puts("If Canada keeps vaccinating at the rate we did today, everyone will be vaccinated by #{date_of_total_vax}")
